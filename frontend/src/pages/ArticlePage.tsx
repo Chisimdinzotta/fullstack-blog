46a79ce0006f90764fce2a51ab83462af9986c7d
Chisimdinzotta <108879532+Chisimdinzotta@users.noreply.github.com>
2023-11-13 01:07:16 +0000

    update article page

--- frontend/src/pages/ArticlePage.tsx
+++ frontend/src/pages/ArticlePage.tsx
@@ -3,39 +3,66 @@ import { useParams } from "react-router-dom";
 import axios from 'axios';
 import Comments from "../components/CommentsList";
 import NotFound from "./NotFoundPage";
-import articleType from "../models/article.model";
+//import articleType from "../models/article.model";
+import articles from "../assets/article.content";
 
 interface ArticleIdType{
     articleId:string;
 }
 
 interface ArticleInfoType{
-    upvote: number;
+    upvotes: number;
     comment: string[];
 }
 
 const ArticlePage: React.FC = ()=>{
 
-    const [articleInfo, setArticleInfo] = useState<ArticleInfoType>({upvote:0, comment:[]});
-    const [Loadidng, setLoading] = useState<Boolean>(true);
+    const [articleInfo, setArticleInfo] = useState<ArticleInfoType>({upvotes:0, comment:[]});
+    const [Loading, setLoading] = useState<Boolean>(true);
     const {articleId}: ArticleIdType = useParams();
 
     useEffect(()=>{
         const loadArticleInfo = async ()=>{
-            const response = await axios.get(`/api/articles/${articleId}`)
-            const newArticlesInfo: ArticleInfoType = response.data;
-            setArticleInfo(newArticlesInfo);
-            setLoading(false);
+            try {
+                const response = await axios.get(`/api/article/${articleId}`)
+                const newArticleInfo: ArticleInfoType = response.data
+                setArticleInfo(newArticleInfo)
+            }
+            catch(error){
+                console.log(`${error} occured while loading article`);
+            }
+            finally{
+                setLoading(false);
+            }
         }
         loadArticleInfo();
-    }, []);
+    }, [])
 
+    const article = articles.find(article => article.title === articleId);
 
+    if (!article){
+        return(
+            <NotFound/>
+        )
+    };
+
+    const addUpVote = async ()=>{
+        const reponse = await axios.put(`//api/article/${articleId}/upvote`);
+        const updatedArticleInfo:ArticleInfoType = reponse.data;
+        setArticleInfo(updatedArticleInfo);
+    };
 
     return(
         <div>
-            <h1>Article Page</h1>
+            {/* add loading logic */}
+            <h1>{article.title}</h1>
+            <div className="">
+                <button onClick={addUpVote}>Upvote</button>
+                <p>This article is {articleInfo.upvotes} vote(s)</p>
+            </div>
+            {article.content.map((paragraph, i)=>(
+                <p key={i}>{paragraph}</p>
+            ))};
+            {/* render comment list */}
         </div>
-    )
-
-};
+    )};
